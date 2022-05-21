6fdee856a086657fc1768ea01c1fc0813ac42c35
wagnervm5 <wagnerj.santos@hotmail.com>
2022-05-21 20:19:41 -0300

    alteração de função, anonima "arrow" não funcionou

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-
-let cpf = document.getElementById('cpf');
-console.log(cpf)
-
-document.getElementById('cpf').addEventListener('blur', () => {
-    
+function formataCpf(){ 
+    let cpf = document.getElementById('cpf');
     let value = cpf.value.replace(/([\d]{3})([\d]{3})([\d]{3})([\d]{2})/, '$1.$2.$3-$4');
         cpf.value = value
-         console.log(value)
-})
+}
